f644dc6cdb4d420d59b1cc903c9a40c3ae966f59
Dony Indiarto <dony.indiarto@gmail.com>
2024-08-29 12:43:54 +0700

    #9 quesb_report_template

--- 05_quesb/rscript/ques_biodiv_basic.r
+++ 05_quesb/rscript/ques_biodiv_basic.r
@@ -9,6 +9,7 @@ library(sf)
 library(DBI)
 library(RSQLite)
 library(ggplot2)
+library(caTools)
 
 # Source custom functions
 source("05_quesb/rscript/ques_biodiv_functions.r")
@@ -47,37 +48,23 @@ quesb_result <- quesb_single_period(
   fragstats_path = fragstats_path
 )
 
+# End of the script
 end_time <- Sys.time()
 cat("Ended at:", format(end_time, "%Y-%m-%d %H:%M:%S"), "\n")
-
-# Copy report template
-file.copy("05_quesb/report_template/quesb_report_template.Rmd",
-          to = file.path(output_dir, "QuES_B_report.Rmd"), overwrite = TRUE)
-
 session_log <- format_session_info_table()
 
-# Prepare parameters for report rendering
-fca_path <- if (is.null(fca_path)) "05_quesb/rscript/teciuf.fca" else fca_path
 
-if (is.null(fragstats_path)) {
-  program_files <- c("C:/Program Files/", "C:/Program Files (x86)/")
-  fragstats_dirs <- list.files(program_files, pattern = "^Fragstats 4", full.names = TRUE)
-  if (length(fragstats_dirs) == 0) {
-    stop("No Fragstats 4.x installation found.")
-  }
-  fragstats_path <- sort(fragstats_dirs, decreasing = TRUE)[1]
-}
-
-params <- list(
-  start_time = format(start_time, "%Y-%m-%d %H:%M:%S"),
-  end_time = format(end_time, "%Y-%m-%d %H:%M:%S"),
+report_params <- list(
+  start_time = as.character(format(start_time, "%Y-%m-%d %H:%M:%S")),
+  end_time = as.character(format(end_time, "%Y-%m-%d %H:%M:%S")),
   output_dir = output_dir,
   total_area = quesb_result$total_area,
   dir_teci_map = basename(quesb_result$path_teci_map),
-  dir_focal_area = basename(quesb_result$path_focal_area),
+  dir_focal_area_ = basename(quesb_result$path_focal_area),
   dir_sampling_grid = basename(quesb_result$path_sampling_grid),
   dir_difa_table = basename(quesb_result$path_difa_table),
   difa_table = quesb_result$difa_table,
+  difa_score = quesb_result$difa_score,
   inputs = list(
     lc_t1_path = lc_t1_path,
     t1 = t1,
@@ -87,12 +74,22 @@ params <- list(
     output_dir = output_dir,
     sampling_points = sampling_points,
     window_size = window_size,
-    window.shape = window.shape,
-    fca_path = fca_path,
-    fragstats_path = fragstats_path
+    window.shape = window.shape
   ),
   session_log = session_log
 )
+
+# Prepare parameters for report rendering
+report_params$inputs$fca_path <- if(is.null(fca_path)) {"05_quesb/rscript/teciuf.fca"} else {fca_path}
+if (is.null(params$inputs$fragstats_path)) {
+  program_files <- c("C:/Program Files/", "C:/Program Files (x86)/")
+  fragstats_dirs <- list.files(program_files, pattern = "^Fragstats 4", full.names = TRUE)
+  if (length(fragstats_dirs) == 0) {
+    stop("No Fragstats 4.x installation found.")
+  }
+  # Sort directories to use the latest version if multiple are found
+  report_params$inputs$fragstats_path <- sort(fragstats_dirs, decreasing = TRUE)[1]
+}
 
 # Render the R Markdown report
 if (!rmarkdown::pandoc_available()) {
@@ -100,8 +97,8 @@ if (!rmarkdown::pandoc_available()) {
 }
 
 rmarkdown::render(
-  input = file.path(output_dir, "QuES_B_report.Rmd"),
+  input = "05_quesb/report_template/quesb_report_template.Rmd",
   output_file = "QuES_B_report.html",
   output_dir = output_dir,
-  params = params
+  params = report_params
 )
