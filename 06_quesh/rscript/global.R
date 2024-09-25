d6c8e60d0d6553c7e311ab7c1632e567305b7567
Faza Mahezs <fazamahezs@gmail.com>
2024-09-25 17:42:08 +0700

    add tab to report

--- 06_quesh/rscript/global.R
+++ 06_quesh/rscript/global.R
@@ -33,6 +33,7 @@ install_load(
   "ggplot2", 
   "scales",
   "tidyr",
+  "tidyterra",
   "DT"
 )
 
@@ -542,4 +543,51 @@ read_shapefile <- function(shp_input) {
     cat("Error occurred:", e$message, "\n")
     stop(paste("Error reading shapefile:", e$message))
   })
+}
+
+#' Plot a categorical raster map
+#'
+#' This function takes a raster object as input and produces a ggplot. If the raster
+#' object includes a "color_pallete" column with hex color codes, these colors are
+#' used for the fill scale. Otherwise, the default `scale_fill_hypso_d()` fill scale
+#' from the tidyterra package is used.
+#'
+#' @param raster_object A raster object.
+#'
+#' @return A ggplot object.
+#' @importFrom tidyterra scale_fill_hypso_d
+#' @importFrom ggplot2 ggplot theme_bw labs theme scale_fill_manual element_text unit element_blank guides guide_legend
+#' @importFrom tidyterra geom_spatraster scale_fill_hypso_d
+#' @export
+plot_categorical_raster <- function(raster_object) {
+  # Check if raster_object has a color_pallete column and it contains hex color codes
+  if ("color_palette" %in% names(cats(raster_object)[[1]]) && all(grepl("^#[0-9A-Fa-f]{6}$", cats(raster_object)$color_pallete))) {
+    fill_scale <- scale_fill_manual(values = cats(raster_object)[[1]]$color_palette, na.value = "white")
+  } else {
+    fill_scale <- scale_fill_manual(values = palette.colors(n = 10, "Tableau 10"), na.value = "white")
+  }
+  if(!is.na(time(raster_object))) {
+    plot_title <- time(raster_object)
+  } else {
+    plot_title <- names(raster_object)
+  }
+  # Generate the plot
+  plot_lc <- ggplot() +
+    geom_spatraster(data = raster_object) +
+    fill_scale +
+    theme_bw() +
+    labs(title = plot_title, fill = NULL) +
+    guides(fill = guide_legend(title.position = "top", ncol=3))+
+    theme(axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          legend.title = element_text(size = 12),
+          legend.text = element_text(size = 10),
+          legend.key.height = unit(0.25, "cm"),
+          legend.key.width = unit(0.25, "cm"),
+          legend.position = "bottom",
+          legend.justification = c(0,0.8))
+  
+  return(plot_lc)
 }
