4ff81d5eb17b631c7e2a710c479f301983365fd4
Yumna Karimah <karimah.yumna@gmail.com>
2024-10-23 18:39:36 +0700

    Minor improvements

--- 11_sciendo-simulate/rscript/app.R
+++ 11_sciendo-simulate/rscript/app.R
@@ -17,6 +17,9 @@ ui <- fluidPage(
   useShinyjs(),
   theme = bs_theme(version = 5),
   extendShinyjs(text = jscode, functions = c("closeWindow")),
+  tags$head(
+    tags$link(rel = "shortcut icon", href = "favicon.ico")  
+  ),
   titlePanel("SCIENDO Simulate"),
   sidebarLayout(
     sidebarPanel(
