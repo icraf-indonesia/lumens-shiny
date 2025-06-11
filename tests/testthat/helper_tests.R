43d410396df48511c7f7cd9523b7f0119b7b7696
Dony Indiarto <dony.indiarto@gmail.com>
2025-06-11 14:23:22 +0700

    Update helper_tests.R
    #47

--- tests/testthat/helper_tests.R
+++ tests/testthat/helper_tests.R
@@ -34,6 +34,52 @@ setup_test_data <- function() {
   )
 }
 
+setup_baliase_test_data <- function() {
+  # Define file paths for shapefile components
+  shp_base_path <- "data/BALIASE/LOKASI_IZIN_PS_BALIASE"
+  admin_paths <- c(
+    paste0(shp_base_path, ".shp"),
+    paste0(shp_base_path, ".shx"),
+    paste0(shp_base_path, ".sbx"),
+    paste0(shp_base_path, ".sbn"),
+    paste0(shp_base_path, ".dbf"),
+    paste0(shp_base_path, ".cpg"),
+    paste0(shp_base_path, ".prj")
+  )
+  
+  # Read and process the shapefile to create the zone raster
+  sf_object <- sf::st_read(admin_paths[1]) %>%
+    dplyr::transmute(planning_unit = paste(SKEMA, LEMBAGA)) %>%
+    dplyr::mutate(ID = dplyr::row_number()) %>%
+    dplyr::select(ID, planning_unit) %>%
+    dplyr::rename(Value = 1, planning_unit = 2)
+  
+  lc_t1_raster <- terra::rast("data/BALIASE/PL2018re.tif")
+  lc_t1_res <- terra::res(lc_t1_raster)
+  zone_raster <- rasterise_multipolygon(sf_object, raster_res = lc_t1_res, field = "Value")
+  zone_raster <- terra::resample(zone_raster, lc_t1_raster, method = "near")
+  
+  list(
+    lc_t1_input = list(
+      datapath = "data/BALIASE/PL2018re.tif",
+      name = "PL2018re"
+    ),
+    lc_t2_input = list(
+      datapath = "data/BALIASE/PL2023re.tif",
+      name = "PL2023re"
+    ),
+    admin_z_input = zone_raster, # Use the pre-processed raster
+    trajectory_lookup_input = list(
+      datapath = "../../data/table/trajectory_loss_recovery.csv"
+    ),
+    lc_lookup_input = list(
+      datapath = "data/BALIASE/Tuplah_Baliase.csv"
+    ),
+    zone_lookup_input = NULL # Not used in this test case
+  )
+}
+
+
 
 
 
@@ -43,3 +89,8 @@ setup_output_dir <- function() {
   return(test_output)
 }
 
+setup_output_dir_baliase <- function() {
+  test_output <- "../../output/ques_pre_baliase"
+  dir.create(test_output, recursive = TRUE, showWarnings = FALSE)
+  return(test_output)
+}
