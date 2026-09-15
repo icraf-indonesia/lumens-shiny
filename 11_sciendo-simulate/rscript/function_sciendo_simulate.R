2af8dcadae12c3929727ab0a9f32d29f3b0f5fde
Dony Indiarto <dony.indiarto@gmail.com>
2026-09-15 11:35:07 +0700

    Restored output_file
    #213

--- 11_sciendo-simulate/rscript/function_sciendo_simulate.R
+++ 11_sciendo-simulate/rscript/function_sciendo_simulate.R
@@ -788,6 +788,8 @@ generate_sciendo_simulate_report <- function(output, dir) {
     template_path <- "../report_template/sciendo_simulate_report_template_INA.Rmd"
   }
 
+  output_file <- paste0("sciendo_simulate_report_", Sys.Date(), ".html")
+
   rmarkdown::render(
     template_path,
     output_file = output_file,
